aa383b0713f7ba24c543b432d7859fed45564c68
rangatron <aleop213@gmail.com>
2021-04-26 17:59:43 +1200

    Attempting to make everything work but i have no clue

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('WP'));
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
