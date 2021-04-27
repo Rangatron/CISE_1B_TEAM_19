433bb9635519088458ee898c922b7babd4af67fd
rangatron <aleop213@gmail.com>
2021-04-27 17:46:38 +1200

    fixing the problem i was  having after realizing i could use mongodb

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const articles = require('./routes/api/articles');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('WP'));
+// Connect Database
+connectDB();
 
-const port = process.env.PORT || 8000;
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/articles', articles);
+
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
